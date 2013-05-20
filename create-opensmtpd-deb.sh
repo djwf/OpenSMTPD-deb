4ef2b03b3c118a7f67b44edee653e7a6423f8ffb
David J. Weller-Fahy <dave@weller-fahy.com>
2013-05-20 01:17:23 -0500

    Major modifications.
    - Fail on error (stop instead of blindly proceeding).
    - Can be used as a cron job if necessary.  Will only create package if
      the most recent commit in the github repository is not contained in a
      package, and should not emit to STDOUT unless an error occurs or a new
      package is being built.
    - Use debian-recommended flags for building package.
    - Use `mktemp` to generate temporary directory under current user's home
      directory.
    - Add files to fill out the package (copyright, README, init scripts,
      runlevel defaults, lintian ignores, changelog).
    - Remove files not needed in debian install.
    - Convert hard links to symbolic links.
    - Strip all the executables.
    - Compress the man pages, and link two aliases to the proper man pages.
    - Use variables to set version and source directory for package building
      command.
    - Divert package build location instead of copying after the fact.
    - Ensure root:root used within package.
    - Identify all three config files (including init.d script).
    - Set pre/post inst/rm scripts.
    - Remove the temporary directory once the package is built.

--- create-opensmtpd-deb.sh
+++ create-opensmtpd-deb.sh
@@ -1,11 +1,93 @@
 #!/bin/sh
+set -e
 
+# Set directory variables.
+odir=$(pwd)
+tdir=$(mktemp -d --tmpdir=$HOME/tmp)
+idir=$tdir/installdir
+gdir=~/usr/src/opensmtpd-portable
+
+# Check to see if the most recent commit is new by comparing timestamp with
+# previously built packages.
+cd $gdir
+git pull -q
+cdate=$(git log -n 1 --date=iso --format=%ci)
+version=$(date --date="$cdate" +%Y%m%d%H%M%S)-42
+test ! -f $odir/opensmtpd-${version}_amd64.deb
+
+# At this point, the package doesn't exist: announce the package version so
+# it shows up at the top of the cron email.
+echo Building OpenSMTPD debian package based on git repository with latest commit:
+echo
+git log -n 1
+echo
+
+# Build everything.
+./bootstrap
+./configure --prefix=/usr --sysconfdir=/etc \
+	CFLAGS="$(dpkg-buildflags --get CFLAGS)" \
+	CPPFLAGS="$(dpkg-buildflags --get CPPFLAGS)" \
+	CXXFLAGS="$(dpkg-buildflags --get CXXFLAGS)" \
+	LDFLAGS="$(dpkg-buildflags --get LDFLAGS)"
+make clean
+make
+mkdir $idir
+make install DESTDIR=$idir
+
+# Place files in etc.
+mkdir -p $idir/etc/default
+cp $odir/etc/default/opensmtpd $idir/etc/default/opensmtpd
+mkdir -p $idir/etc/init.d
+cp $odir/etc/init.d/opensmtpd $idir/etc/init.d/opensmtpd
+
+# Place files in usr.
+mkdir -p $idir/usr/share/doc/opensmtpd
+cp $odir/usr/share/doc/opensmtpd/* $idir/usr/share/doc/opensmtpd/
+mkdir -p $idir/usr/share/lintian/overrides
+cp $odir/usr/share/lintian/overrides/opensmtpd $idir/usr/share/lintian/overrides
+
+# Add README file from the git repository.
+cp $gdir/README.md $idir/usr/share/doc/opensmtpd/
+gzip -9 $idir/usr/share/doc/opensmtpd/README.md
+
+# Add and rename LICENSE file from the git repository.
+cp $gdir/LICENSE $idir/usr/share/doc/opensmtpd/copyright
+
+# Add a changelog for each snapshot.
+cat <<EOF > $idir/usr/share/doc/opensmtpd/changelog.Debian
+opensmtpd ($version) wheezy; urgency=low
+
+  * new snapshot
+
+ -- David J. Weller-Fahy <dave@weller-fahy.com>  $(date -R)
+EOF
+gzip -9 $idir/usr/share/doc/opensmtpd/changelog.Debian
+
+# Remove files not needed for debian install.
+(cd $idir/usr/libexec/opensmtpd && rm -f \
+	backend-queue-null backend-queue-ram backend-queue-stub \
+	backend-table-stub filter-dnsbl filter-monkey filter-stub filter-trace)
+
+# Change hard to symbolic links.
+ln -sf ../libexec/opensmtpd/makemap $idir/usr/bin/newaliases
+ln -sf ../libexec/opensmtpd/makemap $idir/usr/sbin/makemap
+ln -sf ../sbin/smtpctl $idir/usr/bin/mailq
+
+# Strip executables.
+find $idir/usr -type f -executable -exec strip {} \;
+
+# Compress man pages.
+find $idir/usr/share/man/man? -type f -exec gzip -9 {} \;
+
+# Link man pages.
+ln -s smtpctl.8.gz $idir/usr/share/man/man8/mailq.8.gz
+ln -s smtpd.8.gz $idir/usr/share/man/man8/sendmail.8.gz
 
 editor="$EDITOR"
 EDITOR="sed -i -r -e '/^(Vendor: |License: ).*$/d'"
 
-fpm -ef -s dir -t deb -n opensmtpd -v git-20130519121309 -C ~/tmp/installdir \
-	-p opensmtpd-VERSION_ARCH.deb \
+fpm -ef -s dir -t deb -n opensmtpd -v $version -C $idir \
+	-p $odir/opensmtpd-VERSION_ARCH.deb \
 	-d "adduser" \
 	-d "libc6 (>= 2.13-38)" \
 	-d "libdb5.1" \
@@ -15,8 +97,16 @@ fpm -ef -s dir -t deb -n opensmtpd -v git-20130519121309 -C ~/tmp/installdir \
 	-d "libssl1.0.0 (>= 1.0.1e-2)" \
 	-d "zlib1g (>= 1:1.2.7)" \
 	-m "David J. Weller-Fahy <dave@weller-fahy.com>" \
+	--deb-user root \
+	--deb-group root \
 	--category mail \
+	--config-files /etc/default/opensmtpd \
+	--config-files /etc/init.d/opensmtpd \
 	--config-files /etc/smtpd.conf \
+	--after-install $odir/postinst \
+	--before-install $odir/preinst \
+	--after-remove $odir/postrm \
+	--before-remove $odir/prerm \
 	--provides mail-transport-agent \
 	--conflicts mail-transport-agent \
 	--replaces mail-transport-agent \
@@ -29,3 +119,5 @@ with other systems speaking the SMTP protocol." \
 	.
 
 EDITOR="$editor"
+
+rm -r $tdir
