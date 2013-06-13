f7bf4527aab929171385997a982a85e70b74ffe0
David J. Weller-Fahy <dave@weller-fahy.com>
2013-06-12 20:31:42 -0700

    Use vars not files, revamp Makefile/shell script.

--- Makefile
+++ Makefile
@@ -1,17 +1,59 @@
-.PHONY: install uninstall
+ifeq ($(OPENSMTPD),)
+	opensmtpd=$(HOME)/usr/src/OpenSMTPD
+else
+	opensmtpd=$(OPENSMTPD)
+endif
+
+ifeq ($(PACKAGES),)
+	packages=/srv/http/caterva.org/apt/.packages
+else
+	packages=$(PACKAGES)
+endif
+
+prefix=$(DESTDIR)/usr/local
+exec_prefix=$(prefix)
+bindir=$(exec_prefix)/bin
+sysconfdir=$(prefix)/etc
+sysconfsubdir=$(sysconfdir)/OpenSMTPD-deb
 
 INSTALL=install
 RM=rm
-idir=$(DESTDIR)$(exec_prefix)/bin
-locOpenSMTPD=$(shell cat location-OpenSMTPD)
-locpackages=$(shell cat location-packages)
+RMDIR=rmdir
+MV=mv
 
-install: create-opensmtpd-deb.sh
-	test -d $(idir) || mkdir $(idir)
-	$(INSTALL) $< $(idir)
-	sed -e 's!%%packages%%!$(locpackages)!' $(idir)/$< > $(idir)/$<.new
-	sed -e 's!%%OpenSMTPD%%!$(locOpenSMTPD)!' $(idir)/$<.new > $(idir)/$<
-	$(RM) -f $(idir)/$<.new
+all:
 
-uninstall: create-opensmtpd-deb.sh
-	$(RM) -f $(idir)/$<
+install: install-bin install-sysconf
+
+install-bin: create-opensmtpd-deb.sh
+	$(INSTALL) -d $(bindir)
+	$(INSTALL) $< $(bindir)
+	sed -e 's!%%packages%%!$(packages)!' $(bindir)/$< >$(bindir)/$<.new
+	sed -e 's!%%OpenSMTPD%%!$(opensmtpd)!' $(bindir)/$<.new >$(bindir)/$<
+	sed -e 's!%%sysconfdir%%!$(sysconfdir)!' $(bindir)/$< >$(bindir)/$<.new
+	$(MV) $(bindir)/$<.new $(bindir)/$<
+
+install-sysconf: postinst postrm preinst prerm
+	$(INSTALL) -d $(sysconfsubdir)/etc/default
+	$(INSTALL) etc/default/opensmtpd $(sysconfsubdir)/etc/default
+	$(INSTALL) -d $(sysconfsubdir)/etc/init.d
+	$(INSTALL) etc/init.d/opensmtpd $(sysconfsubdir)/etc/init.d
+	$(INSTALL) -d $(sysconfsubdir)/usr/share/doc/opensmtpd
+	$(INSTALL) usr/share/doc/opensmtpd/* \
+		$(sysconfsubdir)/usr/share/doc/opensmtpd
+	$(INSTALL) -d $(sysconfsubdir)/usr/share/lintian/overrides
+	$(INSTALL) usr/share/lintian/overrides/opensmtpd \
+		$(sysconfsubdir)/usr/share/lintian/overrides
+	$(INSTALL) $^ $(sysconfsubdir)
+
+uninstall: uninstall-bin uninstall-sysconf
+
+uninstall-bin: create-opensmtpd-deb.sh
+	$(RM) -f $(bindir)/$<
+	-$(RMDIR) -p $(bindir)
+
+uninstall-sysconf:
+	$(RM) -fr $(sysconfsubdir)
+	-$(RMDIR) -p $(sysconfdir)
+
+.PHONY: all install uninstall
--- create-opensmtpd-deb.sh
+++ create-opensmtpd-deb.sh
@@ -2,18 +2,19 @@
 set -e
 
 # Set directory variables.
-gdir="%%OpenSMTPD%%"
-odir="%%packages%%"
+sdir="%%OpenSMTPD%%"
+pdir="%%packages%%"
+edir="%%sysconfdir%%/OpenSMTPD-dir"
 tdir="$(mktemp -d --tmpdir=$HOME/tmp)"
 idir="$tdir/installdir"
 
 # Check to see if the most recent commit is new by comparing timestamp with
 # previously built packages.
-cd "$gdir"
+cd "$sdir"
 git pull -q
 cdate=$(git log -n 1 --date=iso --format=%ci)
 version=$(date --date="$cdate" -u +%Y%m%d%H%M%S)-42
-test ! -f "$odir"/opensmtpd-${version}_amd64.deb
+test ! -f "$pdir"/opensmtpd-${version}_amd64.deb
 
 # At this point, the package doesn't exist: announce the package version so
 # it shows up at the top of the cron email.
@@ -37,23 +38,23 @@ make install DESTDIR="$idir"
 
 # Place files in etc.
 mkdir -p "$idir"/etc/default
-cp "$odir"/etc/default/opensmtpd "$idir"/etc/default/opensmtpd
+cp "$edir"/etc/default/opensmtpd "$idir"/etc/default/opensmtpd
 mkdir -p "$idir"/etc/init.d
-cp "$odir"/etc/init.d/opensmtpd "$idir"/etc/init.d/opensmtpd
+cp "$edir"/etc/init.d/opensmtpd "$idir"/etc/init.d/opensmtpd
 
 # Place files in usr.
 mkdir -p "$idir"/usr/share/doc/opensmtpd
-cp "$odir"/usr/share/doc/opensmtpd/* "$idir"/usr/share/doc/opensmtpd/
+cp "$edir"/usr/share/doc/opensmtpd/* "$idir"/usr/share/doc/opensmtpd/
 mkdir -p "$idir"/usr/share/lintian/overrides
-cp "$odir"/usr/share/lintian/overrides/opensmtpd \
+cp "$edir"/usr/share/lintian/overrides/opensmtpd \
 	"$idir"/usr/share/lintian/overrides
 
 # Add README file from the git repository.
-cp "$gdir"/README.md "$idir"/usr/share/doc/opensmtpd/
+cp "$sdir"/README.md "$idir"/usr/share/doc/opensmtpd/
 gzip -9 "$idir"/usr/share/doc/opensmtpd/README.md
 
 # Add and rename LICENSE file from the git repository.
-cp "$gdir"/LICENSE "$idir"/usr/share/doc/opensmtpd/copyright
+cp "$sdir"/LICENSE "$idir"/usr/share/doc/opensmtpd/copyright
 
 # Add a changelog for each snapshot.
 cat <<EOF > "$idir"/usr/share/doc/opensmtpd/changelog.Debian
@@ -85,41 +86,37 @@ find "$idir"/usr/share/man/man? -type f -exec gzip -9 {} \;
 ln -s smtpctl.8.gz "$idir"/usr/share/man/man8/mailq.8.gz
 ln -s smtpd.8.gz "$idir"/usr/share/man/man8/sendmail.8.gz
 
-editor="$EDITOR"
-EDITOR="sed -i -r -e '/^(Vendor: |License: ).*$/d'"
-
-fpm -ef -s dir -t deb -n opensmtpd -v $version -C "$idir" \
-	-p "$odir"/opensmtpd-VERSION_ARCH.deb \
-	-d "adduser" \
-	-d "libc6 (>= 2.13-38)" \
-	-d "libdb5.1" \
-	-d "libevent-2.0-5 (>= 2.0.19-stable)" \
-	-d "libevent-core-2.0-5 (>= 2.0.19-stable)" \
-	-d "libevent-openssl-2.0-5 (>= 2.0.19-stable)" \
-	-d "libssl1.0.0 (>= 1.0.1e-2)" \
-	-d "zlib1g (>= 1:1.2.7)" \
-	-m "David J. Weller-Fahy <dave@weller-fahy.com>" \
-	--deb-user root \
-	--deb-group root \
-	--category mail \
-	--config-files /etc/default/opensmtpd \
-	--config-files /etc/init.d/opensmtpd \
-	--config-files /etc/smtpd.conf \
-	--after-install "$odir"/postinst \
-	--before-install "$odir"/preinst \
-	--after-remove "$odir"/postrm \
-	--before-remove "$odir"/prerm \
-	--provides mail-transport-agent \
-	--conflicts mail-transport-agent \
-	--replaces mail-transport-agent \
-	--url https://github.com/poolpOrg/OpenSMTPD \
-	--description "Simple Mail Transfer Protocol daemon
+env EDITOR="sed -i -r -e '/^(Vendor: |License: ).*$/d'" /usr/local/bin/fpm \
+		-ef -s dir -t deb -n opensmtpd -v $version -C "$idir" \
+		-p "$pdir"/opensmtpd-VERSION_ARCH.deb \
+		-d "adduser" \
+		-d "libc6 (>= 2.13-38)" \
+		-d "libdb5.1" \
+		-d "libevent-2.0-5 (>= 2.0.19-stable)" \
+		-d "libevent-core-2.0-5 (>= 2.0.19-stable)" \
+		-d "libevent-openssl-2.0-5 (>= 2.0.19-stable)" \
+		-d "libssl1.0.0 (>= 1.0.1e-2)" \
+		-d "zlib1g (>= 1:1.2.7)" \
+		-m "David J. Weller-Fahy <dave@weller-fahy.com>" \
+		--deb-user root \
+		--deb-group root \
+		--category mail \
+		--config-files /etc/default/opensmtpd \
+		--config-files /etc/init.d/opensmtpd \
+		--config-files /etc/smtpd.conf \
+		--after-install "$edir"/postinst \
+		--before-install "$edir"/preinst \
+		--after-remove "$edir"/postrm \
+		--before-remove "$edir"/prerm \
+		--provides mail-transport-agent \
+		--conflicts mail-transport-agent \
+		--replaces mail-transport-agent \
+		--url https://github.com/poolpOrg/OpenSMTPD \
+		--description "Simple Mail Transfer Protocol daemon
 This is the portable version of OpenSMTPD, a FREE implementation of the
 server-side SMTP protocol as defined by RFC 5321, with some additional
 standard extensions. It allows ordinary machines to exchange e-mails
 with other systems speaking the SMTP protocol." \
-	.
-
-EDITOR="$editor"
+		.
 
 rm -r "$tdir"
